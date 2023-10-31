b0f6e525e42ed0012c961cdd739db993f03fec60
rezaulislamtarek <mrezaulislamtarek.t@gmail.com>
2023-10-31 18:22:51 +0600

    full screen viewpager added

--- Touchly/ContentView.swift
+++ Touchly/ContentView.swift
@@ -9,13 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Home()
     }
 }
 
@@ -23,4 +17,143 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+
+struct Home : View {
+    
+    @State var x : CGFloat = 0
+    @State var count : CGFloat = 0
+    @State var screen = UIScreen.main.bounds.width - 30
+    @State var op : CGFloat = 0
+    
+    @State var data = [
+
+        Card(id: 0, color: .pink, name: "Jill", show: false),
+        Card(id: 1, color: .green, name: "Emma", show: false),
+        Card(id: 2, color: .blue, name: "Catherine", show: false),
+        Card(id: 3, color: .yellow, name: "iJustine", show: false),
+        Card(id: 4, color: .red, name: "Juliana", show: false),
+        Card(id: 5, color: .mint, name: "Lilly", show: false),
+        Card(id: 6, color: .purple, name: "Emily", show: false)
+
+    ]
+    
+    var body : some View{
+        
+            VStack{
+                
+                Spacer()
+                
+                HStack(spacing: 15){
+                    
+                    ForEach(data){i in
+                        
+                        CardView(data: i)
+                        .offset(x: self.x)
+                        .highPriorityGesture(DragGesture()
+                        
+                            .onChanged({ (value) in
+                                
+                                if value.translation.width > 0{
+                                    
+                                    self.x = value.location.x
+                                }
+                                else{
+                                    
+                                    self.x = value.location.x - self.screen
+                                }
+                                
+                            })
+                            .onEnded({ (value) in
+
+                                if value.translation.width > 0{
+                                    
+                                    
+                                    if value.translation.width > ((self.screen - 80) / 2) && Int(self.count) != 0{
+                                        
+                                        
+                                        self.count -= 1
+                                        self.updateHeight(value: Int(self.count))
+                                        self.x = -((self.screen + 15) * self.count)
+                                    }
+                                    else{
+                                        
+                                        self.x = -((self.screen + 15) * self.count)
+                                    }
+                                }
+                                else{
+                                    
+                                    
+                                    if -value.translation.width > ((self.screen - 80) / 2) && Int(self.count) !=  (self.data.count - 1){
+                                        
+                                        self.count += 1
+                                        self.updateHeight(value: Int(self.count))
+                                        self.x = -((self.screen + 15) * self.count)
+                                    }
+                                    else{
+                                        
+                                        self.x = -((self.screen + 15) * self.count)
+                                    }
+                                }
+                            })
+                        )
+                    }
+                }
+                .frame(width: UIScreen.main.bounds.width)
+                .offset(x: self.op)
+                
+                Spacer()
+            }
+            .background(Color.black.opacity(0.07).edgesIgnoringSafeArea(.bottom))
+            .navigationBarTitle("Carousel List")
+            .animation(.spring())
+            .onAppear {
+                
+                self.op = ((self.screen + 15) * CGFloat(self.data.count / 2)) - (self.data.count % 2 == 0 ? ((self.screen + 15) / 2) : 0)
+                
+                self.data[0].show = true
+            }
+        
+    }
+    
+    func updateHeight(value : Int){
+        
+        
+        for i in 0..<data.count{
+            
+            data[i].show = false
+        }
+        
+        data[value].show = true
+    }
+}
+
+struct CardView : View {
+    
+    var data : Card
+    
+    var body : some View{
+        
+        VStack(alignment: .leading, spacing: 0){
+            
+            Text(data.name)
+                .fontWeight(.bold)
+                .padding(.vertical, 13)
+                .padding(.leading)
+            
+        }
+        .frame(width: UIScreen.main.bounds.width - 30, height: data.show ? UIScreen.main.bounds.height : UIScreen.main.bounds.height - 50)
+       // .frame(maxWidth: UIScreen.main.bounds.width - 30, maxHeight: data.show ? .infinity : 440)
+        .background(data.color)
+        .cornerRadius(25)
+    }
+}
+
+struct Card : Identifiable {
+    
+    var id : Int
+    var color : Color
+    var name : String
+    var show : Bool
 }
