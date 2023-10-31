f561a871fe6da6b74a08805c0820b467a76ca664
rezaulislamtarek <mrezaulislamtarek.t@gmail.com>
2023-10-31 17:18:08 +0600

    Initial Commit

---
+++ Touchly/TouchlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchlyApp.swift
+//  Touchly
+//
+//  Created by Rezaul Islam on 31/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TouchlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
